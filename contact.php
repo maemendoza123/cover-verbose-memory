4a1ab52b82ce41a08bf5db670cee024dffd2255d
maemendoza123 <201980004@psu.palawan.edu.ph>
2022-02-24 22:42:50 +0800

    inc directory done

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php include 'inc/header.php'; ?>
+
+
+
+      <main role="main" class="inner cover">
+
+        <h1 class="cover-heading">Contact</h1>
+
+        <p class="lead">The act of touching or connecting with someone or something else is referred to as contact. The question is not so much the if there is other life in the world as it is how we will ever establish contact with it.
+
+        The following are the example of contact numbers:
+
+        Lorwin Bitanga - 09362826852
+        Mae Kristine Mendoza - 09738563917
+        Francis Ceasar Martinez - 09638462842   </p>
+
+        <p class="lead">
+
+          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+          
+        </p>
+      </main>
+
+
+<?php include 'inc/footer.php'; ?>
---
+++ features.php
@@ -0,0 +1,18 @@
+<?php include 'inc/header.php'; ?>
+
+
+      <main role="main" class="inner cover">
+
+        <h1 class="cover-heading">Features</h1>
+
+        <p class="lead">A feature is a distinguishing characteristic or a special attraction. Feature can also mean to draw attention to something in particular. The word feature can also be used as a noun and a verb. A feature is a distinguishing quality or characteristic that something possesses.</p>
+
+        <p class="lead">
+
+          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+
+        </p>
+      </main>
+
+
+<?php include 'inc/footer.php'; ?>
